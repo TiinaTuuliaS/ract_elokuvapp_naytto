4cf6dbc69e4a208d0a89606b983de4d622de2e6e
Tiina Siremaa <tiinatuuliak@gmail.com>
2025-03-01 18:54:01 +0200

    app jsx tyhjennetty valmiista sisällöstä

--- frontti/src/main.jsx
+++ frontti/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+//app injektoidaan sivulle - kaiken alku
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
