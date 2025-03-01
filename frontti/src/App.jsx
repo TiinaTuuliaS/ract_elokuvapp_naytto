265ae092b4ea27b8d85d64531142bfbc9011a7d7
Tiina Siremaa <tiinatuuliak@gmail.com>
2025-03-01 20:01:17 +0200

    luotu sivut kansio ja sen alle home ja favorites tiedostot

--- frontti/src/App.jsx
+++ frontti/src/App.jsx
@@ -5,12 +5,10 @@ import './App.css'
 import MovieCard from './components/MovieCard'
 
 function App() {
+
   return (
    <>
-    <MovieCard movie={{title :"Nupin moovie", releasedate :"2015" }}/>
-    <MovieCard movie={{title :"Nupin moovie 2", releasedate :"2016" }}/>
-    <MovieCard movie={{title :"Nupin moovie 3", releasedate :"2017" }}/>
-    <MovieCard movie={{title :"Nupin moovie final part", releasedate :"2018" }}/>
+
     </>
   );
 
