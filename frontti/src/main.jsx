d89aca3847d6bed6cff3711ca2da0354cf911b4d
Tiina Siremaa <tiinatuuliak@gmail.com>
2025-03-02 18:52:10 +0200

    routerin lisäys, favorites sivun koodausta

--- frontti/src/main.jsx
+++ frontti/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter } from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
 
 
-//app injektoidaan sivulle - kaiken alku
+//app injektoidaan sivulle - kaiken alku, luodaan router 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <BrowserRouter>  
+   <App /> 
+   </BrowserRouter>
   </StrictMode>,
 )
---
+++ frontti/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+
+
+function Favorite() {
+    return <div className= "favorites-empty">
+        <h2>Et ole lisännyt suosikkeja.</h2>
+        <p>Aloita lisäämällä elokuvia suosikkikansioosi niin näet ne täällä!</p>
+    </div>
+}
+
+export default Favorite
